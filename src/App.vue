aa30bd7721cdb5db195084656e981642591d0659
Gavin Alexander <38883412+gavinalexander@users.noreply.github.com>
2021-09-17 12:36:07 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ export default {
 @import url("https://fonts.googleapis.com/css?family=Bangers|Permanent+Marker|Graduate:400,700");
 
 
-// main css test
+// main css
 body {
   //background-color: #999;
   background-color: #0071c5;
